e3c0c296a4e0abe5a79049f2fbd4a00b332e5183
enczilada88 <83005780+enczilada88@users.noreply.github.com>
2021-07-21 18:59:12 +0200

    Update routes.py

--- blog/routes.py
+++ blog/routes.py
@@ -2,6 +2,7 @@ from blog import app
 from flask import render_template, request, flash, session, redirect, url_for
 from blog.models import Entry, db
 from blog.forms import EntryForm
+from blog.forms import *
 
 
 
@@ -14,17 +15,18 @@ def index():
 
 @app.route('/new-post/', methods=['GET', 'POST'])
 def create_entry():
-    return create_or_edit_entry()
+    return create_entry()
 
 
 @app.route('/edit-post/<int:entry_id>', methods=['GET', 'POST'])
 def edit_entry(entry_id: int):
-    return create_or_edit_entry(entry_id=entry_id)
+    return edit_entry(entry_id=entry_id)
 
 
 
+#changes
 
-def create_or_edit_entry(**kwargs):
+def create_entry(**kwargs):
     # GET
     entry_id = kwargs.pop('entry_id', None)
     form = EntryForm()
@@ -33,6 +35,48 @@ def create_or_edit_entry(**kwargs):
     if entry_id:
         entry = Entry.query.filter_by(id=entry_id).first_or_404()
         form = EntryForm(obj=entry)
+
+    # POST
+    if request.method == 'POST':
+        if form.validate_on_submit():
+                entry = Entry(
+                    title=form.title.data,
+                    body=form.body.data,
+                    is_published=form.is_published.data
+                )
+                db.session.add(entry)
+                db.session.commit()
+                flash('Nowy post został dodany. '
+                      'Aby wyświetlić go na stronie głównej pamiętaj aby zaznaczyć "Wpis opublikowany"')
+        else:
+            errors = form.errors
+    return render_template('entry_form.html', form=form, errors=errors)
+
+
+
+
+
+def edit_entry(**kwargs):
+    # GET
+    entry_id = kwargs.pop('entry_id', None)
+    form = EntryForm()
+    entry = None
+    errors = None
+    if entry_id:
+        entry = Entry.query.filter_by(id=entry_id).first_or_404()
+        form = EntryForm(obj=entry)
+
+    # POST
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            if entry_id:
+                form.populate_obj(entry)
+                db.session.commit()
+                flash('Zmiany w poscie zostały zapisane. '
+                      'Aby wyświetlić go na stronie głównej pamiętaj aby zaznaczyć "Wpis opublikowany"')
+        else:
+            errors = form.errors
+    return render_template('entry_form.html', form=form, errors=errors)
 
     # POST
     if request.method == 'POST':
