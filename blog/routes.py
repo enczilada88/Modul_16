ac4dd7212b2fbe75ecdfc19a292f44efda9b5514
enczilada88 <83005780+enczilada88@users.noreply.github.com>
2021-07-21 19:01:02 +0200

    Update routes.py

--- blog/routes.py
+++ blog/routes.py
@@ -28,13 +28,9 @@ def edit_entry(entry_id: int):
 
 def create_entry(**kwargs):
     # GET
-    entry_id = kwargs.pop('entry_id', None)
     form = EntryForm()
     entry = None
     errors = None
-    if entry_id:
-        entry = Entry.query.filter_by(id=entry_id).first_or_404()
-        form = EntryForm(obj=entry)
 
     # POST
     if request.method == 'POST':
@@ -73,28 +69,6 @@ def edit_entry(**kwargs):
                 form.populate_obj(entry)
                 db.session.commit()
                 flash('Zmiany w poscie zostały zapisane. '
-                      'Aby wyświetlić go na stronie głównej pamiętaj aby zaznaczyć "Wpis opublikowany"')
-        else:
-            errors = form.errors
-    return render_template('entry_form.html', form=form, errors=errors)
-
-    # POST
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            if entry_id:
-                form.populate_obj(entry)
-                db.session.commit()
-                flash('Zmiany w poscie zostały zapisane. '
-                      'Aby wyświetlić go na stronie głównej pamiętaj aby zaznaczyć "Wpis opublikowany"')
-            else:
-                entry = Entry(
-                    title=form.title.data,
-                    body=form.body.data,
-                    is_published=form.is_published.data
-                )
-                db.session.add(entry)
-                db.session.commit()
-                flash('Nowy post został dodany. '
                       'Aby wyświetlić go na stronie głównej pamiętaj aby zaznaczyć "Wpis opublikowany"')
         else:
             errors = form.errors
